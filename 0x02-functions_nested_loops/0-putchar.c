3902ddd0d4989a803427c297f515e1d785c8b2cd
avndon <donclark4live@gmail.com>
2022-09-13 16:35:19 -0700

    Nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,7 +10,16 @@
 
 int main(void)
 {
-	write("_putchar\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+
 	return (0);
 }
 
