ea1115b302329b5580470e7cac8922d438b670cd
avndon <donclark4live@gmail.com>
2022-09-13 17:44:51 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main."
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * Description - a program that prints the alphabets in lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	_putchar('\n');
+}
