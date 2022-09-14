3cf69b5a36d6ae5bdaa39a3984ee800fd26f7386
avndon <donclark4live@gmail.com>
2022-09-13 21:47:31 -0700

    Nested loop

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,40 +8,40 @@
 int main(void)
 {
 	int count;
-	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
+	unsigned long fn1 = 0, fn2 = 1, sum;
+	unsigned long fn1_h1, fn1_h2, fn2_h1, fn2_h2;
+	unsigned long h1, h2;
 
 	for (count = 0; count < 92; count++)
 	{
-		sum = fib1 + fib2;
+		sum = fn1 + fn2;
 		printf("%lu, ", sum);
 
-		fib1 = fib2;
-		fib2 = sum;
+		fn1 = fn2;
+		fn2 = sum;
 	}
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
+	fn1_h1 = fn1 / 10000000000;
+	fn2_h1 = fn2 / 10000000000;
+	fn1_h2 = fn1 % 10000000000;
+	fn2_h2 = fn2 % 10000000000;
 
 	for (count = 93; count < 99; count++)
 	{
-		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 + fib2_half2;
+		h1 = fn1_h1 + fn2_h1;
+		h2 = fn1_h2 + fn2_h2;
 
-		if (fib1_half2 + fib2_half2 > 9999999999)
+		if (fn1_h2 + fn2_h2 > 9999999999)
 		{
-			half1 += 1;
-			half2 %= 10000000000;
+			h1 += 1;
+			h2 %= 10000000000;
 		}
-		printf("%lu%lu", half1, half2);
+		printf("%lu%lu", h1, h2);
 		if (count != 98)
 			printf(", ");
-		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
-		fib2_half2 = half2;
+		fn1_h1 = fn2_h1;
+		fn1_h2 = fn2_h2;
+		fn2_h1 = h1;
+		fn2_h2 = h2;
 	}
 	printf("\n");
 	return (0);
