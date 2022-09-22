39d8b998e728bf5d28755aa5dea9982ca5b30425
avndon <donclark4live@gmail.com>
2022-09-21 17:09:45 -0700

    Poimters

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,20 +10,12 @@
 
 char *leet(char *s)
 {
-	int i;
+	int i, temp;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; i < n / 2; ++i)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
 	}
-	return (s);
 }
