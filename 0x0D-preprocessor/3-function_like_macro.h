bd48c64322d98a7a2ba6f2526c5bffd84b2d505d
avndon <donclark4live@gmail.com>
2022-10-10 16:57:13 -0700

    function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef ABS
 #define ABS(num) ((num) > (-1) ? (num) : (num * -1))
 
-#endif /* !FUNCTION_LIKE_MACRO_H */
+#endif /* !ABS */
