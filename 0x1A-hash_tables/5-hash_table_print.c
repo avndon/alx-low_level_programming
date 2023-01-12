368441afa6b4c249cb82d1468a059834c8f2f265
avndon <donclark4live@gmail.com>
2023-01-11 17:17:06 -0800

    edited some files

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -5,7 +5,6 @@
  * @ht: A pointer to the hash table to print.
  *
  * Description: Key/value pairs are printed in the order
-
  *              they appear in the array of the hash table.
  */
 void hash_table_print(const hash_table_t *ht)
